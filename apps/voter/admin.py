c87a6f4ba425ac4b3945a1ea43b8a137c79f117f
olegaver-xxx <oleg.bakiev.jrs@gmail.com>
2026-04-04 10:14:30 +0300

    ...

--- apps/voter/admin.py
+++ apps/voter/admin.py
@@ -3,9 +3,8 @@ from django.contrib import admin
 from apps.voter.models import Voter
 
 
-@admin.register(Voter)
-class VoterAdmin(admin.ModelAdmin):
-    # fields = 'answer'
-    list_display = ('answer',)
+# @admin.register(Voter)
+# class VoterAdmin(admin.ModelAdmin):
+#     list_display = 'answer'
 
 admin.site.register(Voter)
